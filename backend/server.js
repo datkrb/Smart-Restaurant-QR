778badf30b9de2ae41769a5e8ca46058fd209948
datkrb <datkrb@example.com>
2025-12-18 19:09:13 +0700

    phan quyen

--- backend/server.js
+++ backend/server.js
@@ -7,34 +7,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Cấu hình danh sách các domain được phép gọi API
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://smart-restaurant-qr-1.onrender.com/",
-  "http://192.168.197.1:5173/",
-  "http://192.168.44.1:5173/",
-  "http://10.252.20.171:5173/",
-];
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Cho phép request không có origin (như Postman hoặc mobile app)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true, // Quan trọng nếu dùng cookie/session
-  })
-);
+app.use(cors());
 app.use(express.json()); // Cho phép đọc dữ liệu JSON gửi lên
 
 // Kết nối MongoDB
