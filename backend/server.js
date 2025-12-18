1d45351119993d19824c7d9c048112895921280d
datkrb <datkrb@example.com>
2025-12-18 18:04:35 +0700

    fix PORT

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 // Middleware
 app.use(cors()); // Cho phép Frontend gọi API
